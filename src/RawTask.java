6dc913ea0f3e4cd98b8b7a6fafe9feffb1a7dbfc
ranj2004 <jules.randolph@usherbrooke.ca>
2015-06-30 14:14:47 -0400

    Ajout de lombok

---
+++ src/RawTask.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ranj2004 on 15-06-30.
+ */
+public class RawTask {
+
+}
--- src/main.java
+++ src/main.java
@@ -1,53 +1,60 @@
 import java.io.File;
 import java.lang.System;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class main {
 
 	public static void main(String[] args) {
-		Connection c = null;
-		Statement stmt = null;
+        Connection c = null;
+        String defaultdbName = "db_files"+ File.separator +"2015-01-16_14-32_EXP23.db";
 
-		try {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:" + defaultdbName);
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+            parseEnregistrements(c);
+
+            c.close();
+        } catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+	private static void parseEnregistrements(Connection c){
+
+
+        try {
 			/*
 			 * BufferedReader br = new BufferedReader(new InputStreamReader(
 			 * System.in));
 			 * System.out.println("Entrez le nom complet du fichier"); String
 			 * dbName = br.readLine();
 			 */
-			String defaultdbName = "db_files"+ File.separator +"2015-01-16_14-32_EXP23.db";
 
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:" + defaultdbName);
-			c.setAutoCommit(false);
-			System.out.println("Opened database successfully");
+            Statement stmt = null;
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM recording_task;");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String scenarioName = rs.getString("scenario_name");
+                int taskId = rs.getInt("task_id");
+                String state = rs.getString("state");
+                String timestamp = rs.getString("timestamp");
 
-			stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM recording_task;");
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				String scenarioName = rs.getString("scenario_name");
-				int taskId = rs.getInt("task_id");
-				String state = rs.getString("state");
-				String timestamp = rs.getString("timestamp");
+                System.out.println("ID = " + id);
+                System.out.println("scenario name = " + scenarioName);
+                System.out.println("taskId = " + taskId);
+                System.out.println("state = " + state);
+                System.out.println("timestamp = " + timestamp);
 
-				System.out.println("ID = " + id);
-				System.out.println("scenario name = " + scenarioName);
-				System.out.println("taskId = " + taskId);
-				System.out.println("state = " + state);
-				System.out.println("timestamp = " + timestamp);
-
-			}
-			rs.close();
-			stmt.close();
-			c.close();
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
-		}
-		System.out.println("Operation done successfully");
-	}
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
 }
